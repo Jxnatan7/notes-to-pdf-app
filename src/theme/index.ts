cf33e9c496b6aaab8dff5cc8486176745314e406
Jxnatan7 <jgabrieldesouzasantos@gmail.com>
2024-04-26 16:43:42 -0300

    :sparkles: feat: Adiciona configuração do tema da aplicação.

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+import { createBox, createText, createTheme } from "@shopify/restyle";
+
+import { colors } from "./colors";
+import { spacing } from "./spacing";
+import { textVariants } from "./textVariants";
+
+const theme = createTheme({
+    colors,
+    spacing,
+    textVariants,
+});
+
+type ThemeProps = typeof theme;
+
+const Box = createBox<ThemeProps>();
+const Text = createText<ThemeProps>();
+
+export { theme, Box, Text };
+export type { ThemeProps };
---
+++ src/theme/spacing/index.ts
@@ -0,0 +1,10 @@
+const spacing = {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+    xxl: 60,
+    exaggerated: 200,
+};
+
+export { spacing };
